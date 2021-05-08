cc9e28bd128c13d636df9f6a608d0deaaabff259
Lokesh Rathi <38494676+Lokeshrathi@users.noreply.github.com>
2021-05-08 14:35:53 +0530

    Add files via upload

---
+++ details.py
@@ -0,0 +1,11 @@
+user = 'username'
+password = 'password'
+
+comments=['Great Content! Who is the Best Hooper EVER?',
+          'MAGIC or STEPH, who is your favorite?',
+          'Who you think is the GOAT of BASKETBALL?',
+          'Thank God for NBA, Will LAKERS Win 2nd in a row?',
+          'How much would Jordan avg in today?',
+          'Are you In for PLAY-IN tournament?']
+
+search_term = '#NBA'
